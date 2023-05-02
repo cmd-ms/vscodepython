c3ded07e995d2a17e8e67534296139d71c251718
Murat <murat_58sahin@hotmail.com>
2023-05-02 17:53:20 +0100

    exercise 2 done

---
+++ May2023/Week1/Python02.py
@@ -0,0 +1,37 @@
+"""
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
+"""
+# Exercise
+# In this exercise, you will need to add numbers and strings to the correct lists using the "append" list method. 
+# You must add the numbers 1,2, and 3 to the "numbers" list, and the words 'hello' and 'world' to the strings variable.
+# You will also have to fill in the variable second_name with the second name in the names list, using the brackets operator []. 
+# Note that the index is zero-based, so if you want to access the second item in the list, its index will be 1.
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world")
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
