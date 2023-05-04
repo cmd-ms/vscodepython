25882cd490f7522d7ef67f1c045e129a8eafec93
Murat Sahin <murat_58sahin@hotmail.com>
2023-05-05 00:53:45 +0100

    new folders edited during githubs crash

--- May2023/Week1/Python04-1.py
+++ May2023/Week1/Python04-1.py
@@ -1,2 +1,17 @@
-#Conditions
+# Conditions
 
+x = 2
+print(x == 2)  # prints out True
+print(x != 3)  # prints out True
+print(x < 3)  # prints out True
+
+name = "John"
+age = 23
+if name == "John" and age == 23:
+    print("Your name is John, and you are also 23 years old.")
+
+if name == "John" or name == "Rick":
+    print("Your name is either John or Rick.")
+
+if name in ["John", "Rick"]:
+    print("Your name is either John or Rick.")
