fee7a7b330a42ff80eaca2f64c2962c51d6a7d48
Murat Sahin <murat_58sahin@hotmail.com>
2023-05-07 23:00:20 +0100

    Running few tests

---
+++ May2023/Week1/testingNotes.py
@@ -0,0 +1,19 @@
+#this files used for testing
+
+mylist = [1,2,3,5,1,6]
+mylistb = ["new list","hello new list"]
+print([mylist+mylistb])
+if False and False:
+    print("true")
+print(10%2)
+
+a = False
+b = False
+if (a,b) is True:
+    print("this world sucks")
+
+a=[1,2,3,4]
+b=[1]
+a=False
+if not a:
+    print("yhyh")
