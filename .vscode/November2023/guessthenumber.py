facf72e76d30c9239a5df256dca65d1ca3a3bdec
Murat Sahin <murat_58sahin@hotmail.com>
2023-11-22 19:53:25 +0000

    guess the number

--- .vscode/November2023/guessthenumber.py
+++ .vscode/November2023/guessthenumber.py
@@ -10,5 +10,22 @@ def guess(x):
         elif guess < random_number:
             print('Guess again too high...')
     print(f'Yay, congrats. You have guessed the correct number: {random_number}')
+#guess(10)
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #could also be high b/c low = high
+        feedback = input(f'Is {guess}, too high (H), too low (L), or correct (C)?')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess +1
+    print(f'Nice! the computer has guessed your number, {guess}, correctly!')
+
+computer_guess(10000)
